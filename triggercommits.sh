c936a0bc61cf929a9ec3bdde906bd06b5a0fe02a
Peter Juma <wanyonyi.p@gmail.com>
2020-07-18 21:31:54 +0300

    fail

--- triggercommits.sh
+++ triggercommits.sh
@@ -3,11 +3,11 @@ default=1
 count=${1:-$default}
 for (( i=1; i<$count+1; i++))
 do
+	sh ./editfiles.sh File000*
 	COMMIT=$(python3 commitmsg.py)
 	DATE=$(date +%d/%m/%Y@%T)
 	COMMITMSG=$(echo $COMMIT | cut -d"|" -f1)
 	COMMITMSGURL=$(echo $COMMIT | cut -d"|" -f2)
-	sh ./editfiles.sh 
 	git add *
 	git commit -a -m "${COMMITMSG}"
 done
